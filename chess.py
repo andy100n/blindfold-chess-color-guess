de7707ed1208630f1377f883034f00decff04a45
Andrey Stoimenov <andrey.stoimenov@tuhh.de>
2024-06-24 02:52:29 +0200

    Added quality of life

--- chess.py
+++ chess.py
@@ -23,13 +23,13 @@ os.system("cls")
 sq = (random.randint(0,7), random.randint(0,7))
 sq_color = (sq[0] + sq[1]) % 2 
 print(chr(sq[0]+ord('a')) + str(sq[1]+1), '\t\t', '0.0%')
-spoken = chr(sq[0]+ord('a')) + ' ' + str(sq[1]+1)
+spoken = chr(sq[0]+ord('A')) + '-' + str(sq[1]+1)
 speak(spoken)
 
 while True:
     key = keyboard.read_event()
     
-    if key.event_type == keyboard.KEY_DOWN:
+    if key.event_type == keyboard.KEY_UP:
         if key.name == 'esc':
             break
         elif key.name == 'r':
@@ -49,5 +49,5 @@ while True:
             sq = (random.randint(0,7), random.randint(0,7))
             sq_color = (sq[0] + sq[1]) % 2 
             print(chr(sq[0]+ord('a')) + str(sq[1]+1), '\t\t', round(score*100/total, 2), '%')
-            spoken = chr(sq[0]+ord('a')) + ' ' + str(sq[1]+1)
+            spoken = chr(sq[0]+ord('A')) + '-' + str(sq[1]+1)
             speak(spoken)
